368e86f41af8df2482953eec41f9df3d1beb0a97
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2025-12-13 20:59:52 +0300

    grid layout testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="wii-background min-h-screen flex flex-col items-center">
+         <div className="grid grid-cols-4 gap-3 mt-15">
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">1</div>
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">2</div>
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">3</div>
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">4</div>
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">5</div>
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">6</div>
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">7</div>
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">8</div>
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">9</div>
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">10</div>
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">11</div>
+        <div className="wii-radius w-[400px] h-[200px] bg-amber-500">12</div>
+      </div>
     </div>
   )
 }
