ef4cc6bcd211bfed0a0a79d83ff78a99a516c6c9
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2025-12-13 23:09:33 +0300

    Added dock buttons

--- src/components/WiiDock.tsx
+++ src/components/WiiDock.tsx
@@ -1,12 +1,14 @@
 import DockBackground from "./DockBackground";
+import DockButtons from "./DockButtons";
 import DockClock from "./DockClock";
 
 const WiiDock = () => {
   return (
-    <div className="relative w-full h-30  mt-10">
-      <div className="absolute -bottom-20  bg-[#ccced4] w-full h-20" />
+    <div className="relative w-full h-30 mt-10">
+      <div className="absolute -bottom-20 bg-[#ccced4] w-full h-20" />
       <DockBackground />
       <DockClock />
+      <DockButtons />
     </div>
   );
 };
