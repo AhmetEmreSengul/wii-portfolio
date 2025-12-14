4c90ed9d2d3d9c039c0a06e16ea6c17527f3c899
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2025-12-15 02:43:43 +0300

    change delay behavior for video titles

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -21,14 +21,22 @@ const Channel = () => {
             autoPlay
             muted
           />
+
           <AnimatePresence>
             {hoverId === channel.id && (
               <motion.div
-                className="absolute bg-[#e4e4e2] -bottom-16 flex items-center justify-center z-2 w-110 h-15  rounded-full shadow-xl"
+                className="absolute bg-[#e4e4e2] -bottom-16 flex items-center justify-center z-2 w-110 h-15 rounded-full shadow-xl"
                 initial={{ opacity: 0, scale: 0.9 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.2, delay: 0.3 }}
-                exit={{ opacity: 0, scale: 0.9 }}
+                animate={{
+                  opacity: 1,
+                  scale: 1,
+                  transition: { duration: 0.2, delay: 0.4 }, 
+                }}
+                exit={{
+                  opacity: 0,
+                  scale: 0.9,
+                  transition: { duration: 0.2 }, 
+                }}
               >
                 <p className="tracking-wide text-[#494949] text-3xl font-semibold">
                   {channel.title}
