476cb44df564ca847c5a4c84d5f301d96855bbd2
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2025-12-16 02:54:44 +0300

    refactor Channel component to improve state management and enhance hover effects; update ChannelPreview to accept hoverId prop

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -4,69 +4,96 @@ import { AnimatePresence, motion } from "framer-motion";
 import ChannelPreview from "./ChannelPreview";
 
 const Channel = () => {
-  const [hoverId, setHoverId] = useState<number>(0);
-  const [clickId, setClickId] = useState<number>(0);
+  const [hoverId, setHoverId] = useState<number | null>(null);
+  const [activeChannel, setActiveChannel] = useState<number | null>(null);
 
   const videoRefs = useRef<Record<number, HTMLVideoElement | null>>({});
 
-  const handleClick = (id: number) => {
-    Object.values(videoRefs.current).forEach((video) => {
-      video?.pause();
-    });
-    setClickId(id);
+  const openChannel = (id: number) => {
+    Object.values(videoRefs.current).forEach((v) => v?.pause());
+    setActiveChannel(id);
   };
 
-  const handleClose = () => {
-    setClickId(0);
-    Object.values(videoRefs.current).forEach((video) => {
-      video?.play();
-    });
+  const closeChannel = () => {
+    setActiveChannel(null);
   };
-
-  console.log(hoverId);
 
   return (
-    <div className="grid grid-cols-4 mt-9">
-      {channePreview.map((channel) => (
-        <div
-          className="w-105 h-55 flex flex-col items-center relative -mx-1 -my-2"
-          onMouseEnter={() => setHoverId(channel.id)}
-          onMouseLeave={() => setHoverId(0)}
-          onClick={() => handleClick(channel.id)}
-          key={channel.id}
-        >
-          <ChannelPreview channel={channel} videoRefs={videoRefs} />
-          <AnimatePresence>
-            {hoverId === channel.id && (
-              <motion.div
-                className="absolute bg-[#e4e4e2] -bottom-16 flex items-center justify-center z-2 w-110 h-15 rounded-full shadow-xl"
-                initial={{ opacity: 0, scale: 0.9 }}
-                animate={{
-                  opacity: 1,
-                  scale: 1,
-                  transition: { duration: 0.2, delay: 0.4 },
-                }}
-                exit={{
-                  opacity: 0,
-                  scale: 0.9,
-                  transition: { duration: 0.2 },
-                }}
-              >
-                <p className="tracking-wide text-[#494949] text-3xl font-semibold">
-                  {channel.title}
-                </p>
-              </motion.div>
-            )}
-          </AnimatePresence>
+    <>
+      <div className="grid grid-cols-4 mt-9">
+        {channePreview.map((channel) => (
+          <div
+            key={channel.id}
+            className="w-105 h-55 flex flex-col items-center relative -mx-1 -my-2"
+            onMouseEnter={() => setHoverId(channel.id)}
+            onMouseLeave={() => setHoverId(null)}
+            onClick={() => openChannel(channel.id)}
+          >
+            <ChannelPreview
+              channel={channel}
+              hoverId={hoverId}
+              videoRefs={videoRefs}
+            />
 
-          {clickId === channel.id && (
-            <div className="inset-0 fixed h-screen w-screen flex items-center justify-center z-3">
-              <video src={channel.video} autoPlay loop />
-            </div>
-          )}
-        </div>
-      ))}
-    </div>
+            <AnimatePresence>
+              {hoverId === channel.id && !activeChannel && (
+                <motion.div
+                  initial={{ opacity: 0, y: 8 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 8 }}
+                  transition={{ duration: 0.2 }}
+                  className="absolute bg-[#e4e4e2] -bottom-16 z-2 w-110 h-15 rounded-full shadow-xl flex items-center justify-center"
+                >
+                  <p className="tracking-wide text-[#494949] text-3xl font-semibold">
+                    {channel.title}
+                  </p>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+        ))}
+      </div>
+
+      <AnimatePresence
+        mode="wait"
+        onExitComplete={() => {
+          Object.values(videoRefs.current).forEach((v) => v?.play());
+        }}
+      >
+        {activeChannel !== null && (
+          <motion.div
+            key="overlay"
+            className="fixed inset-0 z-10 flex items-center justify-center"
+            transition={{ duration: 0.5 }}
+          >
+            <motion.div
+              layoutId={`video-${activeChannel}`}
+              className="w-full h-full flex flex-col justify-end"
+            >
+              <video
+                src={channePreview.find((c) => c.id === activeChannel)?.video}
+                autoPlay
+                className="w-full h-full object-contain"
+                onClick={(e) => e.stopPropagation()}
+              />
+
+              <div className="w-full h-50 fixed border-t-2 border-black channel-background flex items-center justify-center gap-5">
+                <button
+                  className="w-140 text-6xl h-30 border-4 bg-[#e3e8ef] border-[#34beed] rounded-full relative"
+                  onClick={closeChannel}
+                >
+                  Wii Menu
+                </button>
+
+                <button className="w-140 text-6xl h-30 border-4 border-[#34beed] bg-[#e3e8ef] rounded-full inset-shadow-2xs">
+                  Start
+                </button>
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
--- src/components/ChannelPreview.tsx
+++ src/components/ChannelPreview.tsx
@@ -1,11 +1,17 @@
+import { motion } from "framer-motion";
 import type { RefObject } from "react";
 
 interface ChannelPreviewProps {
   channel: { id: number; title: string; video: string };
   videoRefs: RefObject<Record<number, HTMLVideoElement | null>>;
+  hoverId: number | null;
 }
 
-const ChannelPreview = ({ channel, videoRefs }: ChannelPreviewProps) => {
+const ChannelPreview = ({
+  channel,
+  videoRefs,
+  hoverId,
+}: ChannelPreviewProps) => {
   return (
     <svg
       viewBox="-4 -4 108 48"
@@ -27,12 +33,14 @@ const ChannelPreview = ({ channel, videoRefs }: ChannelPreviewProps) => {
         width="108"
         height="48"
         mask={`url(#mask-${channel.id})`}
-        className="pointer-events-auto"
+        className="pointer-events-auto bg-black"
       >
-        <video
-          ref={(e) => {
-            videoRefs.current[channel.id] = e;
+        <motion.video
+          layoutId={`video-${channel.id}`}
+          ref={(el) => {
+            videoRefs.current[channel.id] = el;
           }}
+          transition={{ duration: 0.3 }}
           className="w-full h-full object-cover"
           src={channel.video}
           loop
@@ -45,7 +53,7 @@ const ChannelPreview = ({ channel, videoRefs }: ChannelPreviewProps) => {
       <path
         d="M 6 0 C 35 0 65 0 94 0 C 97 0 100 3 100 6 C 101 16 101 24 100 34 C 100 37 97 40 94 40 C 65 41 35 41 6 40 C 3 40 0 37 0 34 C -1 24 -1 15 0 6 C 0 3 3 0 6 0 Z"
         fill="none"
-        stroke="#b3b3b3"
+        stroke={hoverId === channel.id ? "#6bc8f3" : "#b9bac2"}
         strokeWidth="1.2"
         className="pointer-events-none"
       />
