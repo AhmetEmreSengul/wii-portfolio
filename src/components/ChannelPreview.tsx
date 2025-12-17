5ffe787f009a9ae06c55a3ae2f6070451cf7cdb1
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2025-12-17 22:30:20 +0300

    Add new audio files for video interactions

--- src/components/ChannelPreview.tsx
+++ src/components/ChannelPreview.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import type { RefObject } from "react";
+import { useEffect, useRef, type RefObject } from "react";
 
 interface ChannelPreviewProps {
   channel: { id: number; title: string; video: string };
@@ -12,52 +12,57 @@ const ChannelPreview = ({
   videoRefs,
   hoverId,
 }: ChannelPreviewProps) => {
+  const audio = new Audio("/sounds/video-open.mp3");
+  audio.volume = 0.3;
+
   return (
-    <svg
-      viewBox="-4 -4 108 48"
-      preserveAspectRatio="none"
-      className="absolute inset-0 w-full h-full pointer-events-none"
-    >
-      <defs>
-        <mask id={`mask-${channel.id}`}>
-          <path
-            d="M 6 0 C 35 0 65 0 94 0 C 97 0 100 3 100 6 C 101 16 101 24 100 34 C 100 37 97 40 94 40 C 65 41 35 41 6 40 C 3 40 0 37 0 34 C -1 24 -1 15 0 6 C 0 3 3 0 6 0 Z"
-            fill="white"
-          />
-        </mask>
-      </defs>
-
-      <foreignObject
-        x="-4"
-        y="-4"
-        width="108"
-        height="48"
-        mask={`url(#mask-${channel.id})`}
-        className="pointer-events-auto bg-black"
+    <div onClick={() => audio.play()}>
+      <svg
+        viewBox="-4 -4 108 48"
+        preserveAspectRatio="none"
+        className="absolute inset-0 w-full h-full pointer-events-none"
       >
-        <motion.video
-          layoutId={`video-${channel.id}`}
-          ref={(el) => {
-            videoRefs.current[channel.id] = el;
-          }}
-          transition={{ duration: 0.3 }}
-          className="w-full h-full object-cover"
-          src={channel.video}
-          loop
-          autoPlay
-          muted
-          playsInline
-        />
-      </foreignObject>
+        <defs>
+          <mask id={`mask-${channel.id}`}>
+            <path
+              d="M 6 0 C 35 0 65 0 94 0 C 97 0 100 3 100 6 C 101 16 101 24 100 34 C 100 37 97 40 94 40 C 65 41 35 41 6 40 C 3 40 0 37 0 34 C -1 24 -1 15 0 6 C 0 3 3 0 6 0 Z"
+              fill="white"
+            />
+          </mask>
+        </defs>
 
-      <path
-        d="M 6 0 C 35 0 65 0 94 0 C 97 0 100 3 100 6 C 101 16 101 24 100 34 C 100 37 97 40 94 40 C 65 41 35 41 6 40 C 3 40 0 37 0 34 C -1 24 -1 15 0 6 C 0 3 3 0 6 0 Z"
-        fill="none"
-        stroke={hoverId === channel.id ? "#6bc8f3" : "#b9bac2"}
-        strokeWidth="1.2"
-        className="pointer-events-none"
-      />
-    </svg>
+        <foreignObject
+          x="-4"
+          y="-4"
+          width="108"
+          height="48"
+          mask={`url(#mask-${channel.id})`}
+          className="pointer-events-auto bg-black"
+        >
+          <motion.video
+            layoutId={`video-${channel.id}`}
+            ref={(el) => {
+              videoRefs.current[channel.id] = el;
+            }}
+            transition={{ duration: 0.3 }}
+            className="w-full h-full object-cover"
+            src={channel.video}
+            loop
+            autoPlay
+            muted
+            playsInline
+          />
+        </foreignObject>
+
+        <path
+          d="M 6 0 C 35 0 65 0 94 0 C 97 0 100 3 100 6 C 101 16 101 24 100 34 C 100 37 97 40 94 40 C 65 41 35 41 6 40 C 3 40 0 37 0 34 C -1 24 -1 15 0 6 C 0 3 3 0 6 0 Z"
+          fill="none"
+          stroke={hoverId === channel.id ? "#6bc8f3" : "#b9bac2"}
+          strokeWidth="1.2"
+          className="pointer-events-none"
+        />
+      </svg>
+    </div>
   );
 };
 
